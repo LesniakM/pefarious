550b99726358a68e06a656e1583e0f7154092edd
Mariusz Leśniak <lesniak.mar@gmail.com>
2022-08-04 18:35:56 +0200

    Initial commit for file with card class.

---
+++ cards.py
@@ -0,0 +1,21 @@
+# ID : (AVAILABILITY, NAME, COST, WIN POINTS, (EFFECT1), (EFFECT2))
+
+invention_cards_data = {00: [0, "N", "C", "WP", ("TARGET", "TYPE", "AMOUNT"), ("TARGET", "TYPE", "AMOUNT")],
+                        12: [1, "Uzależniająca gra", 0, 1, ("Personal", "Cash", 4)],
+                        13: [1, "Cybernetyczne zwierzątko", 0, 1, ("Personal", "card", 1, "cash", 2)],
+                        14: [1, "Maszyna PKiKzM", 0, 2],
+                        18: [1, "Urządzenie maskujące", 4, 3],
+                        19: [1, "Afrodyzjaki", 7, 2, ("Personal", "Cash", 8)],
+                        27: [1, "Zabójcze pszczoły", 8, 4],
+                        28: [1, "Plaga szarańczy", 8, 4],
+                        42: [1, "Generator trzesień ziemi", 12, 5],
+                        43: [1, "Generator huraganów", 12, 5],
+                        44: [1, "Zamrażacz pokrywy lodowej", 12, 5],
+                        47: [1, "Generator fal pływowych", 12, 5],
+                        48: [1, "Wyzwalacz wulkanów", 12, 5],
+                        54: [1, "Wytwornica śniegu", 16, 6],
+                        55: [1, "Wielka orbitalna asteroida", 16, 6],
+                        56: [1, "Grawitacja czarnej dziury", 16, 6],
+                        57: [1, "Promienie śmierci", 16, 6],
+                        58: [1, "Potwór z głębin", 16, 6],
+                        59: [1, "Reduktor ozonu", 16, 6]}
