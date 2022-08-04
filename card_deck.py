c89fd21954b0dcf6f7e295bbf90da78a656d4c10
Mariusz Leśniak <lesniak.mar@gmail.com>
2022-08-04 19:36:51 +0200

    Card objects added. Not implemented in game or player yet.

---
+++ card_deck.py
@@ -0,0 +1,33 @@
+from cards import card_parser, Card, invention_cards_data
+from collections import deque
+from random import shuffle
+
+
+class CardDeck:
+    def __init__(self):
+        self.card_stack = deque()
+        self.rejected_card_stack = deque()
+
+        # Make cards and shuffle them. GC will delete card_objs list after CardDeck init.
+        card_objs = [card_parser(card_id, invention_cards_data[card_id]) for card_id in invention_cards_data.keys()]
+        shuffle(card_objs)
+        for card in card_objs:
+            self.card_stack.append(card)
+
+    def get_card(self) -> Card or None:
+        if self.card_stack:
+            return self.card_stack.pop()
+        else:
+            self.reuse_rejected_cards()
+            if self.card_stack:
+                return self.card_stack.pop()
+            else:
+                print("No more cards on board")
+                return None
+
+    def reject_card(self, card: Card) -> None:
+        self.rejected_card_stack.appendleft(card)  # append left coz we want FiFo here.
+
+    def reuse_rejected_cards(self) -> None:
+        self.card_stack = self.rejected_card_stack
+        self.rejected_card_stack.clear()
--- cards.py
+++ cards.py
@@ -1,8 +1,10 @@
-# ID : (AVAILABILITY, NAME, COST, WIN POINTS, (EFFECT1), (EFFECT2))
+from player import Player
 
+
+# ID : (AVAILABILITY, NAME, COST, WIN POINTS, (EFFECT1), (EFFECT2))
 invention_cards_data = {00: [0, "N", "C", "WP", ("TARGET", "TYPE", "AMOUNT"), ("TARGET", "TYPE", "AMOUNT")],
-                        12: [1, "Uzależniająca gra", 0, 1, ("Personal", "Cash", 4)],
-                        13: [1, "Cybernetyczne zwierzątko", 0, 1, ("Personal", "card", 1, "cash", 2)],
+                        12: [1, "Uzależniająca gra", 0, 1, ("player", "cash", 4)],
+                        13: [1, "Cybernetyczne zwierzątko", 0, 1, ("player", "card", 1, "cash", 2)],
                         14: [1, "Maszyna PKiKzM", 0, 2],
                         18: [1, "Urządzenie maskujące", 4, 3],
                         19: [1, "Afrodyzjaki", 7, 2, ("Personal", "Cash", 8)],
@@ -19,3 +21,28 @@ invention_cards_data = {00: [0, "N", "C", "WP", ("TARGET", "TYPE", "AMOUNT"), ("
                         57: [1, "Promienie śmierci", 16, 6],
                         58: [1, "Potwór z głębin", 16, 6],
                         59: [1, "Reduktor ozonu", 16, 6]}
+
+
+class Card:
+    def __init__(self, card_id: int, name: str, cost: int, win_points: int, effect_1=None, effect_2=None):
+        self.id = card_id
+        self.name = name
+        self.cost = cost
+        self.win_points = win_points
+        self.effect_1 = effect_1
+        self.effect_2 = effect_2
+
+    def execute_effects(self, player: Player, enemies: list[Player]) -> None:
+        if self.effect_1:
+            pass
+        if self.effect_2:
+            for enemy in enemies:
+                pass
+        raise NotImplementedError
+
+
+def card_parser(card_id: int, data: list) -> Card:
+    if len(data) == 4: return Card(card_id, data[1], data[2], data[3])                      # No effects
+    if len(data) == 5: return Card(card_id, data[1], data[2], data[3], data[4])             # 1 effect
+    if len(data) == 6: return Card(card_id, data[1], data[2], data[3], data[4], data[5])    # 2 effects
+    raise ValueError("Wrong card data")
