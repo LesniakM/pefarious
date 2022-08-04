9d8ae09ff469f43fed6bfba8632cc18735758c47
Mariusz Leśniak <lesniak.mar@gmail.com>
2022-08-04 18:36:49 +0200

    Added import cards.py

--- game.py
+++ game.py
@@ -1,36 +1,18 @@
 from random import randint
-
 from player import Player, Bot
+from cards import invention_cards_data
+
 
 class Game:
     def __init__(self, players):
         self.players = []
         self.winner = ""
-        # ID : (AVAILABILITY, NAME, COST, WINPOINTS, (EFFECT1), (EFFECT2))
-        self.invention_cards = {00: [0, "N", "C", "WP", ("TARGET", "TYPE", "AMOUNT"), ("TARGET", "TYPE", "AMOUNT")],
-                                12: [1, "Uzależniająca gra", 0, 1, ("Personal", "Cash", 4)],
-                                13: [1, "Cybernetyczne zwierzątko", 0, 1, ("Personal", "card", 1, "cash", 2)],
-                                14: [1, "Maszyna PKiKzM", 0, 2],
-                                18: [1, "Urządzenie maskujące", 4, 3],
-                                19: [1, "Afrodyzjaki", 7, 2, ("Personal", "Cash", 8)],
-                                27: [1, "Zabójcze pszczoły", 8, 4],
-                                28: [1, "Plaga szarańczy", 8, 4],
-                                42: [1, "Generator trzesień ziemi", 12, 5],
-                                43: [1, "Generator huraganów", 12, 5],
-                                44: [1, "Zamrażacz pokrywy lodowej", 12, 5],
-                                47: [1, "Generator fal pływowych", 12, 5],
-                                48: [1, "Wyzwalacz wulkanów", 12, 5],
-                                54: [1, "Wytwornica śniegu", 16, 6],
-                                55: [1, "Wielka orbitalna asteroida", 16, 6],
-                                56: [1, "Grawitacja czarnej dziury", 16, 6],
-                                57: [1, "Promienie śmierci", 16, 6],
-                                58: [1, "Potwór z głębin", 16, 6],
-                                59: [1, "Reduktor ozonu", 16, 6]}
+        self.invention_cards = invention_cards_data
         self.work_income = 4
         self.research_income = 2
         self.research_cards = 1
         self.spy_income = 1
-        self.spy_cost = [0, 0, 2, 0, 1] # indexed with actions
+        self.spy_cost = ["_", 0, 2, 0, 1]  # indexed with actions
         self.start_money = 10
         self.start_cards = 3
         self.points_to_win = 20       
